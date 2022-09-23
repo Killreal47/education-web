a7fd38f6998823b9d3b85079e9e2722c6db67184
Kirill Rusinov <kirill.rusinov190820@mail.ru>
2022-09-23 09:33:20 +0300

    Добавил задачу по уроку 19

--- education.js
+++ education.js
@@ -3,5 +3,20 @@
 
 
 // Задачи по Js
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-console.log(5);
+// let personalMovieDB = {
+// 	count : numberOfFilms,
+// 	movies : {},
+// 	actors: {},
+// 	genres : [],
+// 	privat : false
+// };
+
+// let onceFilm = prompt('Один из последних просмотренных фильмов?', '');
+// let balsFilm = prompt('На сколько балов оцените его?', '');
+
+// personalMovieDB.movies[onceFilm] = balsFilm ;
+
+// console.log(personalMovieDB);
+
