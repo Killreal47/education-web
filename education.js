418a1a0ef42720a347e059f0d7ce1a1d4424ceb9
Kirill Rusinov <kirill.rusinov190820@mail.ru>
2022-09-24 20:50:22 +0300

    задания 21 урок (логич. операторы)

--- education.js
+++ education.js
@@ -3,8 +3,11 @@
 
 
 // Задачи по Js
-// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
+//__________________________________________________
+//                19 УРОК
+
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 // let personalMovieDB = {
 // 	count : numberOfFilms,
 // 	movies : {},
@@ -19,4 +22,29 @@
 // personalMovieDB.movies[onceFilm] = balsFilm ;
 
 // console.log(personalMovieDB);
+
+// 21 Урок Логические операторы
+//____________________________________________________
+
+// && - выводит первое ложное 
+// || - выводит первое истинное false - если оба ложных; true - оба истинных
+
+// console.log( NaN || 2 || undefined ); //2 +
+ 
+// console.log( NaN && 2 && undefined ); // NaN +
+ 
+// console.log( 1 && 2 && 3 ); // 3 +
+ 
+// console.log( !1 && 2 || !3 ); // 2 - false
+ 
+// console.log( 25 || null && !3 ); // 25 +
+ 
+// console.log( NaN || null || !3 || undefined || 5); // 5 +
+ 
+// console.log( NaN || null && !3 && undefined || 5); // 5 +
+ 
+// console.log( 5 === 5 && 3 > 1 || 5); // true +
+
+// console.log( !4 );
+
 
